5bb114f3c6fe3180f47bb657dd47eb555b7bb35a
Arjan van Vught <arjan.van.vught@gmail.com>
2022-10-10 19:52:23 +0200

    V1.1
    *. Added support for display

--- lib-spiflashinstall/src/gd32/spiflashinstall.cpp
+++ lib-spiflashinstall/src/gd32/spiflashinstall.cpp
@@ -2,7 +2,7 @@
  * @file spiflashinstall.cpp
  *
  */
-/* Copyright (C) 2021-2022 by Arjan van Vught mailto:info@gd32-dmx.org
+/* Copyright (C) 2021-2022 by Arjan van Vught mailto:info@gd32-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
